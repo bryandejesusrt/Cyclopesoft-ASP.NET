307e8b7b89cadfac986f2b999b3f75a90877fb94
Bryan-r15 <bryant012482003@gmail.com>
2022-11-22 17:57:30 -0500

    add

--- Cyclopesoft.Conceptual/Model/CyclopesoftModel.Designer.cs
+++ Cyclopesoft.Conceptual/Model/CyclopesoftModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Wex_2\Source\Repos\Cyclopesoft-ASP.NET\Cyclopesoft.Conceptual\Model\CyclopesoftModel.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\bryan\Videos\Cyclopesoft\Cyclopesoft.Conceptual\Model\CyclopesoftModel.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
