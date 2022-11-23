781a7432ae339477c7dbeb59c8c7a9d48f88bfd1
Wex_2 <willensonr.g@gmail.com>
2022-11-23 14:28:06 -0400

    the service layer basicac structure was made

---
+++ Cyclopesoft.ServicesLayer/Contracts/IClientService.cs
@@ -0,0 +1,11 @@
+﻿using Cyclopesoft.ServicesLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cyclopesoft.ServicesLayer.Contracts
+{
+    public interface IClientService : IBaseService
+    {
+    }
+}
